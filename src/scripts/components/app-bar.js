12f973919396f5b067f916c6e932013e1bd616c2
Rifky Alfarez <207006052@student.unsil.ac.id>
2024-10-04 21:34:46 +0700

    Add responsive navigation menu

--- src/scripts/components/app-bar.js
+++ src/scripts/components/app-bar.js
@@ -5,17 +5,44 @@ class AppBar extends HTMLElement {
 
   render() {
     this.innerHTML = `
-       
+      <header>
+      <div class="app-bar">
+        <h1 class="logo">RifkyResto.</h1>
+        <nav class="desktop-nav">
+          <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="#">Favorite</a></li>
+            <li>
+              <a href="https://www.linkedin.com/in/rifkyalfarez">About Us</a>
+            </li>
+          </ul>
+        </nav>
+        <div class="hamburger" onclick="navToggle()">
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+      <nav class="mobile-nav">
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="#">Favorite</a></li>
+          <li>
+            <a href="https://www.linkedin.com/in/rifkyalfarez">About Us</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
       `;
   }
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-  const hamburgerMenu = document.querySelector('.hamburger-menu');
+function navToggle() {
+  const nav = document.querySelector('.mobile-nav');
+  const hamburger = document.querySelector('.hamburger');
 
-  hamburgerMenu.addEventListener('click', () => {
-    hamburgerMenu.classList.toggle('open');
-  });
-});
+  nav.classList.toggle('active');
+  hamburger.classList.toggle('active');
+}
 
 customElements.define('app-bar', AppBar);
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,5 +2,5 @@ import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.css';
 import '../styles/responsive.css';
 import '../styles/reset.css';
-// import './components/app-bar';
+import './components/app-bar';
 // import './components/app-hero';
